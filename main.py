2b8c02db19798834cca40110f54dca14ffcd8bb9
ZSiKLAO <zsiklao@gmail.com.com>
2025-09-14 23:43:29 +0200

    addedd function to count character occurrences in main.py and stats.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from stats import get_book_text, get_num_words
+from stats import get_book_text, get_num_words, get_char_counts
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text("/home/zsikl/workspace/github.com/ZSiKLAO/bookbot/bookbot/books/frankenstein.txt")
     num_words = get_num_words(book_text)
     print(f"{num_words} words found in the document")
+
+    char_counts = get_char_counts(book_text)
+    for char, count in sorted(char_counts.items()):
+        print(f"'{char}': {count}")
+
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -7,3 +7,15 @@ def get_book_text(filepath):
 def get_num_words(text):  # renamed for clarity
     words = text.split()
     return len(words)
+
+def get_char_counts(text):
+    text = text.lower()  # Normalize to lowercase
+    char_counts = {}
+
+    for char in text:
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+
+    return char_counts
