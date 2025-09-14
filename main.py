6835a3ad8e83b3b9cf8117ac0ab720a006818c49
ZSiKLAO <zsiklao@gmail.com.com>
2025-09-15 00:54:31 +0200

    added function to sort character counts in stats.py and imported to main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from stats import get_book_text, get_num_words, get_char_counts
+from stats import get_book_text, get_num_words, get_char_counts, sort_char_counts
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
-    print(f"{num_words} words found in the document")
+    print(f"{num_words} words found in the document\n")
 
     char_counts = get_char_counts(book_text)
-    for char, count in sorted(char_counts.items()):
-        print(f"'{char}': {count}")
+    sorted_chars = sort_char_counts(char_counts)
 
+    for entry in sorted_chars:
+        char = entry["char"]
+        count = entry["num"]
+        if char.isalpha():
+            print(f"The '{char}' character was found {count} times")
 
 if __name__ == "__main__":
     main()
